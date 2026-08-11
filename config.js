e37488fbcd0eebdde0a3b89c9f66954715fe70b9
nslabtmu <nslabtmu@gmail.com>
2026-08-11 15:26:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 // config.js 共用設定檔
-const SUPABASE_CONFIG = {
-    URL: 'https://ligckapfksiyvcvsppde.supabase.co/',
-    ANON_KEY: 'sb_publishable_oDFhgWFOd80vTh8FFanFrw_6Z243ThZ'
-};
+const SUPABASE_URL = 'https://ligckapfksiyvcvsppde.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_oDFhgWFOd80vTh8FFanFrw_6Z243ThZ';
