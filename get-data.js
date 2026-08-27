09bd19b9b48c2c36dd2999bb52574464f5339ff4
nslabtmu <nslabtmu@gmail.com>
2026-08-27 21:06:15 +0800

    Update get-data.js

--- get-data.js
+++ get-data.js
@@ -2,7 +2,7 @@ async function sendData() {
   const input = document.getElementById('userInput').value;
   
   // 填寫你 Cloudflare Pages 的 API 完整網址
-  const res = await fetch('https://your-project.pages.dev/api/save', {
+  const res = await fetch('https://https://headache0811.nslabtmu.workers.dev/api/save', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ message: input })
