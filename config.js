cf51c16a8af1c01b95e9a9293b384de8fd10cc25
nslabtmu <nslabtmu@gmail.com>
2026-09-10 22:54:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,4 @@
 const SUPABASE_URL = 'https://ligckapfksiyvcvsppde.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_oDFhgWFOd80vTh8FFanFrw_6Z243ThZ';
 // 建立全域的 supabase 實例
-// ============ 2. 防重複宣告保護 ============
-// 如果 window 裡面還沒有建立過客戶端，才進行初始化
-// 2. 直接掛載到 window.supabase，如果已經存在就不會重複建立
-if (!window.supabaseClient) {
-    window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-}
-const supabase = window.supabaseClient;
+window.supabase = window.supabase || supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
