a061e2607033c0643e097a5cc21cef346a31033c
nslabtmu <nslabtmu@gmail.com>
2026-09-10 22:59:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+console.log("👉 config.js 開始執行");
 // config.js 共用設定檔
 const SUPABASE_URL = 'https://ligckapfksiyvcvsppde.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_oDFhgWFOd80vTh8FFanFrw_6Z243ThZ';
