0320f156696786fea7306d041f88fa4e566e59e5
nslabtmu <nslabtmu@gmail.com>
2026-09-10 21:52:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js 共用設定檔
-// config.js 共用設定檔
 const SUPABASE_URL = 'https://ligckapfksiyvcvsppde.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_oDFhgWFOd80vTh8FFanFrw_6Z243ThZ';
-
+// 建立全域的 supabase 實例
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
