bd5641647e7dc29ae07ef5d3b36b407d45779e57
nslabtmu <nslabtmu@gmail.com>
2026-09-15 10:33:02 +0800

    Update music.js

--- music.js
+++ music.js
@@ -6,7 +6,7 @@
 const musicConfigs = {
     piano: {
         name: '🎹 鋼琴',
-        url: 'https://youtu.be/7ex3DkT-tog', // 直接貼網址就好！
+        url: 'https://www.youtube.com/watch?v=uNDsZz2vaOQ', // 直接貼網址就好！
         description: '拍速緩慢、留白較多，引導大腦放鬆。'
     },
     forest: {
